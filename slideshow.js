6ea58bf380f268abd2dfc7da7a0ab2c42a893817
William Poon <willphk@outlook.com>
2023-02-22 10:38:18 -0500

    Added js

--- slideshow.js
+++ slideshow.js
@@ -1,1 +1,41 @@
+let currentSlide = 0;
+let slides = document.getElementsByClassName("slide");
 
+function next() {
+  console.log("next");
+  if (currentSlide === slides.length) {
+    currentSlide = 0;
+  } else currentSlide++;
+  setSlide(currentSlide);
+}
+
+function previous() {
+  console.log("previous");
+  if (currentSlide === -1) {
+    currentSlide = slides.length - 1;
+  } else currentSlide--;
+  setSlide(currentSlide);
+}
+
+function setSlide(i) {
+  console.log("set slide");
+  currentSlide = i;
+  hideAllSlides();
+  showSlide(currentSlide);
+  updateCounter();
+}
+
+function updateCounter() {
+  let counter = document.getElementsByClassName("counter");
+  counter[0].innerHTML = currentSlide + 1 + "/" + slides.length;
+}
+
+function hideAllSlides() {
+  for (let i = 0; i < slides.length; i++) {
+    slides[i].style.display = "none";
+  }
+}
+
+function showSlide(i) {
+  slides[i].style.display = "block";
+}
